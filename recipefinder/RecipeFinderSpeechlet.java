da7d96a80ffffe0320f33b0344ceb8e2de806c67
yidingalan <y2366wan@uwaterloo.ca>
2016-11-05 14:09:10 -0400

    setup request handlers

---
+++ recipefinder/RecipeFinderSpeechlet.java
@@ -0,0 +1,76 @@
+package recipefinder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+
+//Handle speechlet request
+
+public class HelloWorldSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(recipeFinderSpeechlet.class);
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any initialization logic goes here
+    }
+
+    //When Alexa gets launhed
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        //return getWelcomeResponse();
+    }
+
+    //When Alexa gets asked questions
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        //If the user is giving the correct intent input
+        if ("GetRecipeIntent".equals(intentName)) {
+            return GetRecipeResponse();
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+
+    /**
+    * logic for getting the recipes
+    * SpeechletResponse: defines the text to speak to the user
+    */
+    private SpeechletResponse GetRecipeResponse(){
+        
+
+    }
